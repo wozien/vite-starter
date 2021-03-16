01f9eb9f892bda6718ece4d7f85ccc178d571238
wozien <571455773@qq.com>
2021-03-16 18:54:31 +0800

    setup rfc feature

--- src/views/test/Test.vue
+++ src/views/test/Test.vue
@@ -3,29 +3,11 @@
   <van-button @click="onClick">Add</van-button>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, watchEffect, watch, onUpdated } from 'vue'
 
-export default defineComponent({
-  setup() {
-    const count = ref(0)
-    const onClick = () => count.value++
+<script lang="ts" setup>
+import { ref } from 'vue';
 
-    watchEffect((onInvalidate) => { 
-      console.log('watchEffect ' + count.value)
-      onInvalidate(() => {
-        console.log('watchEffect invalidate ' + count.value)
-      })
-    }, { onTrigger: (e) => console.log(e)})
-    
-    watch(count, (val: number) => console.log('watch ' + val))
-
-    onUpdated(() => console.log('update hook ' + count.value))
-
-    return {
-      count,
-      onClick
-    }
-  }
-})
+// setup rfc state
+const count = ref(0)
+const onClick = () => count.value++ 
 </script>
